34ecd93e9cadd8e096707ed33e68038f03638ce2
Stefano Straga <stefanostraga2k18@gmail.com>
2023-02-25 11:46:34 +0100

    upload ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
-// import Image from 'next/image'
+import Image from 'next/image'
+import data from '../data.json'
 
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Testing app</h1>;
+  return (
+    <div className="flex items-center">
+      <Image
+        alt={data.name}
+        src={data.avatar}
+        width={48}
+        height={48}
+      />
+    </div>
+  );
 }
