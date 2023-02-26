50b69d242d35ab2c41de3e4c8b99f67631637f3a
Stefano Straga <stefanostraga2k18@gmail.com>
2023-02-26 14:50:40 +0100

    upload index

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,17 +22,19 @@ export default function Home() {
 function LinkCard({ href, title, image}: {href: string; title: string; image?: string}) {
   return (
     <a href={href} className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 mb-3">
-      <div className="flex text-center">
-        {image && (
-          <Image
-            className="rounded-sm"
-            alt={title}
-            src={image}
-            width={40}
-            height={40}
-          />
-        )}
-        <h2 className="font-semibold w-full text-center text-gray-700">{title}</h2>
+      <div className="flex text-center w-full">
+        <div className="w-10 h-10">
+          {image && (
+            <Image
+              className="rounded-sm"
+              alt={title}
+              src={image}
+              width={40}
+              height={40}
+            />
+          )}
+        </div>
+        <h2 className="flex justify-center items-center font-semibold w-full text-gray-700 -ml-10">{title}</h2>
       </div>
     </a>
   );
