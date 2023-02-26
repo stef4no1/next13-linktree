87a8b3aa59cbfd8adb453cda07624e1e0fa2429b
Stefano Straga <stefanostraga2k18@gmail.com>
2023-02-26 14:28:54 +0100

    upload index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import data from '../data.json'
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
       <Image
         className="rounded-full"
         alt={data.name}
@@ -11,7 +11,7 @@ export default function Home() {
         width={96}
         height={96}
       />
-      <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
+      <h1 className="font-bold mt-4 mb-8 text-xl">{data.name}</h1>
       {data.links.map((link) => (
         <LinkCard key={link.href} {...link}/>
       ))}
@@ -21,9 +21,18 @@ export default function Home() {
 
 function LinkCard({ href, title, image}: {href: string; title: string; image?: string}) {
   return (
-    <a href={href} className="flex items-center p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="flex flex-col">
-        <h2 className="font-bold text-lg">{title}</h2>
+    <a href={href} className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all border border-gray-300 mb-3">
+      <div className="flex text-center">
+        {image && (
+          <Image
+            className="rounded-sm"
+            alt={title}
+            src={image}
+            width={40}
+            height={40}
+          />
+        )}
+        <h2 className="font-semibold w-full text-center">{title}</h2>
       </div>
     </a>
   );
