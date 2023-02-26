82877bb20cac0691636e0cec523b474f3325a2bc
Stefano Straga <stefanostraga2k18@gmail.com>
2023-02-26 14:33:08 +0100

    add document

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import {Html, Head, Main, NextScript} from 'next/document';
+
+export default function Document() {
+    return (
+        <Html lang="es">
+            <Head />
+            <body className="bg-gradient-to-r from-gray-900 to-gray-600">
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
+}
